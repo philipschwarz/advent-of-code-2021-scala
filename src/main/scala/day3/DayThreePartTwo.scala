879a953adee7c9637adba043d2dc0b59699dc3ba
philipschwarz <philip.johann.schwarz@gmail.com>
2021-12-11 19:58:49 +0000

    improvements

--- src/main/scala/day3/DayThreePartTwo.scala
+++ src/main/scala/day3/DayThreePartTwo.scala
@@ -9,32 +9,31 @@ import scala.util.{Failure, Success, Try}
   @main def day3Part2: Unit = {
     for
       diagnostics <- getDiagnostics(diagnosticsFileName)
-      aggregatedDiagnostics <- aggregate(diagnostics)
-      oxygenGeneratorRating <- computeOxygenGeneratorRating(diagnostics, aggregatedDiagnostics)
-      carbonDioxideScrubberRating <- computeCarbonDioxideScrubberRating(diagnostics, aggregatedDiagnostics)
+      oxygenGeneratorRating <- computeOxygenGeneratorRating(diagnostics)
+      carbonDioxideScrubberRating <- computeCarbonDioxideScrubberRating(diagnostics)
       lifeSupportRating = oxygenGeneratorRating * carbonDioxideScrubberRating
-    yield reportResult(lifeSupportRating )
+    yield reportResult(lifeSupportRating)
   } recover( handleFailure(_) )
 
-  def computeOxygenGeneratorRating(diagnostics: List[BitCounts], aggregatedDiagnostics: BitCounts, bitNumber: Int = 0): Try[Int] =
+  def computeOxygenGeneratorRating(diagnostics: List[BitCounts], bitNumber: Int = 0): Try[Int] =
     if diagnostics.size == 1 then diagnostics.head.toInt
-    else
+    else aggregate(diagnostics).flatMap{ aggregatedDiagnostics =>
       val filteredDiagnostics =
         if aggregatedDiagnostics.isMostlySet(bitNumber) || aggregatedDiagnostics.isEquallySetAndClear(bitNumber)
         then diagnostics.filter(_.isSet(bitNumber))
         else diagnostics.filter(_.isClear(bitNumber))
-      aggregate(filteredDiagnostics).flatMap{ filteredAggregatedDiagnostics =>
-        computeOxygenGeneratorRating(filteredDiagnostics, filteredAggregatedDiagnostics, bitNumber + 1) }
+      computeOxygenGeneratorRating(filteredDiagnostics, bitNumber + 1)
+    }
 
-  def computeCarbonDioxideScrubberRating(diagnostics: List[BitCounts], aggregatedDiagnostics: BitCounts, bitNumber: Int = 0): Try[Int] =
+  def computeCarbonDioxideScrubberRating(diagnostics: List[BitCounts], bitNumber: Int = 0): Try[Int] =
     if diagnostics.size == 1 then diagnostics.head.toInt
-    else
+    else aggregate(diagnostics).flatMap { aggregatedDiagnostics =>
       val filteredDiagnostics =
         if aggregatedDiagnostics.isMostlySet(bitNumber) || aggregatedDiagnostics.isEquallySetAndClear(bitNumber)
         then diagnostics.filter(_.isClear(bitNumber))
         else diagnostics.filter(_.isSet(bitNumber))
-      aggregate(filteredDiagnostics).flatMap{ filteredAggregatedDiagnostics =>
-        computeCarbonDioxideScrubberRating(filteredDiagnostics, filteredAggregatedDiagnostics, bitNumber + 1) }
+      computeCarbonDioxideScrubberRating(filteredDiagnostics, bitNumber + 1)
+    }
 
   def reportResult(lifeSupportRating: Int): Unit =
-      println(s"The life support rating of the submarine is $lifeSupportRating.")
+    println(s"The life support rating of the submarine is $lifeSupportRating.")
